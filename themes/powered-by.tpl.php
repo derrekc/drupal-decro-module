01b7052b919c73c8486d5fb6cfe39a15a91b0324
Derrek Croney <derrek@decro.net>
2014-08-14 15:43:55 -0400

    added files for 'decro' module

---
+++ themes/powered-by.tpl.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @file
+ */
+
+?>
+
+<div>
+	<span>Copyright &copy; 2014 decro.net</span>
+	&nbsp;|&nbsp;<span><a href="#">Donate (coming soon)</a><br />
+	<?php if(in_array('administrator', $user->roles)): ?>
+		<br /><a class="btn btn-sm btn-info" href="/www_logs">View Site Stats</a>
+	<?php endif; ?>
+	
+	
+</div>	
+
